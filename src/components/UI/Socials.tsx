a66d96e092c4927b2a95478819c3c8a125cf566a
Dhairya <dhairya.rathod24@gmail.com>
2022-11-12 22:55:48 +0530

    Profile link and css changes

--- src/components/UI/Socials.tsx
+++ src/components/UI/Socials.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import Link from "next/link";
 
 import githubIcon from "../../../public/images/social/github.svg";
 import instagramIcon from "../../../public/images/social/instagram.svg";
@@ -6,12 +7,27 @@ import linkedinIcon from "../../../public/images/social/linkedin.svg";
 import twitterIcon from "../../../public/images/social/twitter.svg";
 
 const Socials = () => {
+  const githubUrl: string = process.env.NEXT_PUBLIC_GITHUB_PROFILE as string;
+  const linkedinUrl: string = process.env
+    .NEXT_PUBLIC_LINKEDIN_PROFILE as string;
+  const instagramUrl: string = process.env
+    .NEXT_PUBLIC_INSTAGRAM_PROFILE as string;
+  const twitterUrl: string = process.env.NEXT_PUBLIC_TWITTER_PROFILE as string;
+
   return (
     <div className="flex gap-8 mt-5 ">
-      <Image src={githubIcon} alt="github icon" />
-      <Image src={twitterIcon} alt="twitter icon" />
-      <Image src={linkedinIcon} alt="linkedin icon" />
-      <Image src={instagramIcon} alt="instagram icon" />
+      <Link href={githubUrl} target="_blank">
+        <Image src={githubIcon} alt="github icon" />
+      </Link>
+      <Link href={linkedinUrl} target="_blank">
+        <Image src={linkedinIcon} alt="linkedin icon" />
+      </Link>
+      <Link href={twitterUrl} target="_blank">
+        <Image src={twitterIcon} alt="twitter icon" />
+      </Link>
+      <Link href={instagramUrl} target="_blank">
+        <Image src={instagramIcon} alt="instagram icon" />
+      </Link>
     </div>
   );
 };
