aa31c0f348480674970732d131c3158ea0a674d7
Dhairya <dhairya.rathod24@gmail.com>
2022-11-26 11:56:58 +0530

    deleted icon index file

--- src/components/Icons/Index.tsx
+++
@@ -1,7 +0,0 @@
-export { default as IconMail } from "./Mail";
-export { default as IconGithub } from "./Github";
-export { default as IconLinkedin } from "./Linkedin";
-export { default as IconInstagram } from "./Instagram";
-export { default as IconTwitter } from "./Twitter";
-export { default as IconExternal } from "./External";
-export { default as Icon } from "./Icon";
